4a1219b8b459efe6161782ec70ab0071689049e4
4nnoy1ng <daniel.tuertscher@student.htldornbirn.at>
2021-11-24 16:14:08 +0100

    finish

--- Passwortgenerator/Passwortgenerator.js
+++ Passwortgenerator/Passwortgenerator.js
@@ -1,28 +1,30 @@
 
 var signs = ['!', '"', '§', '$', '%', '&', '/', '(', ')', '=', '', '+', '-'];
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var anzahlzeichen = 8;
 
 
-function lenght() {
-    let lenght1 = document.getElementById("lenght").value;
+
+
+function Click()
+{
+    var lenght1 = document.getElementById("lenght").value;
+    generate(lenght1);
 }
 
-
-function generate(length1,) {
-    console.log("clicked")
-
+function generate(length1){
+   
     var result = '';
     for (var i = 0; i < length1; i++) {
-        let random = Math.random() * 1
+        let random = Math.random() * 1;
         if (random > 0.5) {
-            result += letters[Math.floor(Math.random() * letters.length)];
-
+            result += ""+letters[Math.floor(Math.random() * letters.length)];
         } else {
-            result += signs[Math.floor(Math.random() * signs.length)];
-
+            result += ""+signs[Math.floor(Math.random() * signs.length)];
         }
+       
     }
+    console.log(result);
+   document.getElementById("result1").innerHTML = result;
 }
+    console.log("clicked")
 
-console.log(signs, letters);
